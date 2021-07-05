d6a6511683d9d0398251f3c89338603ca28f1ee2
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-07-05 19:40:19 +0200

    build: experimental nix support
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Go kit extensions";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ go_1_16 golangci-lint gotestsum gnumake ];
+        };
+      });
+}
