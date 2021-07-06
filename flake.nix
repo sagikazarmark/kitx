603101e823804a9a1878155b10acb4248fc3b066
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-07-06 10:37:32 +0200

    build: improve nix environment
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -6,10 +6,28 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        buildDeps = with pkgs; [ git go gnumake ];
+        devDeps = with pkgs; buildDeps ++ [ golangci-lint gotestsum ];
+
+        generateGoEnv = go:
+          pkgs.buildEnv {
+            name = "go" + go.version;
+            paths = (pkgs.lib.remove pkgs.go devDeps) ++ [ go ];
+          };
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ go_1_16 golangci-lint gotestsum gnumake ];
+          buildInputs = devDeps;
+
+          shellHook = ''
+            echo -e "Welcome to the developer console!\n"
+            echo "Available make commands:"
+            make
+          '';
         };
+
+        packages.go1_15 = generateGoEnv pkgs.go_1_15;
+        packages.go1_16 = generateGoEnv pkgs.go_1_16;
       });
 }
